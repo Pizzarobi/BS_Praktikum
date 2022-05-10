5b55600e7e6cbb9261f3f8ee521ec6be80c6a63b
Robert Kalmar <user@127.0.0.1>
2022-05-10 22:48:28 +0200

    Primzahl.c Functioning with time Tests

--- primzahl.c
+++ primzahl.c
@@ -13,7 +13,7 @@ struct primeRange{
 
 int primeAlg(int x){
     int i;
-    for(i = 2; x-1;i++){
+    for(i = 2; i<x-1;i++){
         if((x%i)==0){
             return 0;
         }
@@ -25,15 +25,20 @@ void *calcPrimes(void *args){
     struct primeRange* aRange = (struct primeRange*)args;
     int x;
 
+    printf("aRange->start = %10d, aRange->end = %d\n",aRange->start,aRange->end);
+
     // Check every number in Range
-    for (x = aRange->start; x<aRange->end;x++){
+    for (x = aRange->start; x < (aRange->end); x++){
         // Prime Algorithn
-        printf("Testing: %d =", x);
         int res = primeAlg(x);
-        printf(" %d\n",res);
+        // if(res)
+        //     printf("Testing: %d = %d\n", x, res);
+   
     }
 
-    return aRange;
+    printf("Thread done to %10d\n",aRange->end);
+
+    pthread_exit(NULL);
 }
 
 // Main
@@ -42,11 +47,41 @@ void *calcPrimes(void *args){
 // either print Ranges or return in Array?
 
 int main(){
-    pthread_t thread;
+    // count variables
+    int i = 0;
 
-    struct primeRange aRange;
-    aRange.start = 1;
-    aRange.end = 500;
+    // specify complete amount of numbers starting from 1
+    const int endNumber = 500000;
+    const int threadAmount = 10;
+    float perThread = (float)endNumber / threadAmount;
+
+    // Range Array
+    struct primeRange aRange[threadAmount];
+    // thread Array
+    pthread_t thread[threadAmount];
+
+    // Initialize Ranges and start threads ???
+    for(i = 0; i < threadAmount; i++){
+        aRange[i].start = i*perThread+1;
+        aRange[i].end = i*perThread+perThread;
+        // Set individual ranges
+        printf("aRange[%d].start =  %10d\n",i,aRange[i].start);
+        printf("aRange[%d].end   =  %10d\n",i,aRange[i].end);
+    }
+
+    // force last aRange[] to endNumber
+    aRange[threadAmount-1].end = endNumber;
+    printf("aRange[%d].end   =  %10d\n",threadAmount-1,aRange[threadAmount-1].end);
+
+    // start threads
+    for(i = 0; i < threadAmount; i++){
+        pthread_create(&thread[i], NULL, calcPrimes, (void * )&aRange[i]);
+    }
+    
+    // Waiting for threads to end
+    for(i = 0; i < threadAmount; i++){
+        pthread_join(thread[i],NULL);
+    }
 
     // Starte Thread mit Funktion calcPrimes
     //pthread_create(&thread, NULL, &calcPrimes, &aRange);
@@ -55,11 +90,28 @@ int main(){
     //int *retValue;
     //pthread_join(thread,(void**)(&retValue));
 
-
-    // TEST CODE VON primeAlg
-    printf("3, %d\n",primeAlg(3));
-    printf("7, %d\n",primeAlg(7));
-    printf("10, %d\n",primeAlg(10));
-
     return 0;
 }
+
+
+// TIMES
+// endNumber = 500000
+
+// threads   = 1
+// time      = 33.69s user 0.26s system 99% cpu 33.957 total
+
+// threads   = 2
+// time      = 34.84s user 0.29s system 137% cpu 25.502 total
+// time      = 28.29s user 0.00s system 138% cpu 20.451 total
+
+// threads   = 3
+// time      =
+
+// threads   = 4
+// time      = 29.34s user 0.00s system 242% cpu 12.123 total
+
+//thread     = 10
+// time      = 45.81s user 0.03s system 571% cpu 8.023 total
+
+// threads   = 16
+// time      = 50.01s user 0.01s system 710% cpu 7.038 total
